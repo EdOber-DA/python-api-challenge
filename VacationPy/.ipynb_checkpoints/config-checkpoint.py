539a5c630c31e617f22cd48a75365b4d02ec469f
ECHO <edward.ober@gmail.com>
2021-02-08 11:36:58 -0500

    Vacation working for heat map and for the selection of cities

---
+++ VacationPy/.ipynb_checkpoints/config-checkpoint.py
@@ -0,0 +1,34 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "OpenWeatherMap_api_key = \"2a5c7eaee3e42aae17f7ae3b1d0b9f8a\""
+   ]
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.6.10"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 4
+}
